792ee1f9084cd308d49de9f64d9762a60c4a7c89
George Higbie <gehigbie@gmail.com>
2017-08-18 13:55:10 +0000

    Write skeleton for controller

---
+++ controllers/PersonDetailsController.js
@@ -0,0 +1,12 @@
+/* global angular */
+
+(function(){
+    var PersonListController = function($scope){
+        
+    };
+    
+    PersonListController.$inject('$scope');
+    
+    angular.module('contactsApp')
+        .controller('PersonListController', PersonListController);
+}());
